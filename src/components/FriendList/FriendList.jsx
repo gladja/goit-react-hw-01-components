9cba89ba7eb26cf68a55e7e7b358088e4771df69
gladja <gladkih.alexander@gmail.com>
2023-09-03 00:15:06 +0300

    task03

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,16 +2,26 @@ import PropTypes from 'prop-types';
 import { List, Img, Status } from './FriendList.styled';
 
 export const FriendList = ({ friends }) => {
-  console.log(friends);
   return (
     <List>
-      {friends.map(itm =>(
+      {friends.map(itm => (
         <li key={itm.id}>
-          <Status color={itm.isOnline ? "green" : "red"}></Status>
-          <Img src={itm.avatar}/>
+          <Status color={itm.isOnline ? 'green' : 'red'}></Status>
+          <Img src={itm.avatar} />
           <span>{itm.name}</span>
         </li>
       ))}
     </List>
-  )
-}
+  );
+};
+
+FriendList.protoType = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
